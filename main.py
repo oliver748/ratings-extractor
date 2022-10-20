5703ef0d69df131aa2eca7ae1d51c99e8460697c
Oliver <113297002+oliver748@users.noreply.github.com>
2022-10-20 19:37:26 +0200

    safety that actually works, more readable, cleaner

--- main.py
+++ main.py
@@ -1,59 +1,60 @@
 import requests
 from lxml import html
 from draw import draw_spreadsheet
+import time
 
 
 HEADERS = {"User-Agent": "Mozilla/5.0"}
-SAFETY = True
+OUTPUT_INFO = True # controls if the process should be printed
+SAFETY = True # should be kept on; makes process take longer but ensures all ratings are retrieved
+SLEEP_TIME = 0.75 # time it sleeps with safety enabled - 1.5 secs recommended
 
 def fetch_show(tv_show, season_num):
-    its = 1
-    if SAFETY:
-        its = 2
-    for i in range(its):
-        try:
-            url = f"https://www.imdb.com/title/{tv_show}/episodes?season={season_num}"
-            fetch_webpage = requests.get(url, headers=HEADERS)
-            tree = html.fromstring(fetch_webpage.content)
-            return tree
-        except:
-            pass
+    url = f"https://www.imdb.com/title/{tv_show}/episodes?season={season_num}"
+    fetch_webpage = requests.get(url, headers=HEADERS)
+    tree = html.fromstring(fetch_webpage.content)
+    return tree
 
 
 def fetch_seasons_amount(tv_show):
-    i = 1
     tree = fetch_show(tv_show, 1)
-    while 1:
+    count = 1
+    while True:
         try:
-            xpath = f'//*[@id="bySeason"]/option[{i}]'
+            xpath = f'//*[@id="bySeason"]/option[{count}]'
             season = tree.xpath(xpath)
             season = season[0].text
-            i += 1
+            count += 1
         except:
-            return i-1
+            return count-1
 
 
 def fetch_ratings(tv_show, amount):
-    all_ratings = {}
+    all_ratings = {} # all ratings from every episode
     for i in range(amount):
         print(f'Gathering season number {i+1} out of {amount}')
         tree = fetch_show(tv_show, i+1)
-        season_ratings = []
-        j = 0
-        while 1:
+        season_ratings = [] # all ratings from every episode in specific season
+        count = 1
+        while True: # finds rating for each episode - continues until it cant find more episodes
             try:
-                xpath = f'//*[@id="episodes_content"]/div[2]/div[2]/div[{j+1}]/div[2]/div[2]/div[1]/span[2]'
+                xpath = f'//*[@id="episodes_content"]/div[2]/div[2]/div[{count}]/div[2]/div[2]/div[1]/span[2]'
                 episode_rating = tree.xpath(xpath)
                 episode_rating = episode_rating[0].text
                 season_ratings.append(episode_rating)
             except:
                 break
-            j += 1
+            count += 1
         all_ratings[i+1] = season_ratings
+        
+        if SAFETY: 
+            time.sleep(SLEEP_TIME)
+            
     return all_ratings
 
 
 if __name__ == "__main__":
+    print('NOTE: Close spreadsheet if opened\n')
     tv_show = input('TV Show (IMDB ID - example: tt0460681): ')
     seasons_amount = fetch_seasons_amount(tv_show)
     ratings = fetch_ratings(tv_show, seasons_amount)
