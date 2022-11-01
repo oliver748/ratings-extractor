b6792d4aa1640c182dd67d946662eeddd1a59739
Oliver <oliver@olivers-macbook.local>
2022-11-01 12:30:08 +0100

    added example + little update on main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from time import sleep
 from lxml import html
 from draw import draw_spreadsheet
 
+
 HEADERS = {"User-Agent": "Mozilla/5.0"}
 OUTPUT_INFO = True  # controls if the process should be printed
 SAFETY = True  # makes process longer though ensures all ratings are retrieved
@@ -13,17 +14,18 @@ def fetch_show(tv_show, season_num):
     url = f"https://www.imdb.com/title/{tv_show}/episodes?season={season_num}"
     fetch_web_page = requests.get(url, headers=HEADERS)
     tree = html.fromstring(fetch_web_page.content)
+    if SAFETY:
+        sleep(SLEEP_TIME)
     return tree
 
 
-def fetch_seasons_amount(tv_show):
+def fetch_total_seasons(tv_show):
     tree = fetch_show(tv_show, 1)
     count = 1
     while True:
         try:
             xpath = f'//*[@id="bySeason"]/option[{count}]'
-            season = tree.xpath(xpath)
-            season = season[0].text
+            tree.xpath(xpath)[0].text  # checks if season can be found
             count += 1
         except IndexError:
             return count - 1
@@ -40,23 +42,18 @@ def fetch_ratings(tv_show, amount):
         while True:  # finds rating for each episode till no more episodes
             try:
                 xpath = f'//*[@id="episodes_content"]/div[2]/div[2]/div[{count}]/div[2]/div[2]/div[1]/span[2]'
-                episode_rating = tree.xpath(xpath)
-                episode_rating = episode_rating[0].text
+                episode_rating = tree.xpath(xpath)[0].text
                 season_ratings.append(episode_rating)
             except IndexError:
                 break
             count += 1
         all_ratings[i + 1] = season_ratings
-
-        if SAFETY:
-            sleep(SLEEP_TIME)
-
     return all_ratings
 
 
 if __name__ == "__main__":
     print("NOTE: Close spreadsheet if opened\n")
     tv_show = input("TV Show (IMDB ID - example: tt0460681): ")
-    seasons_amount = fetch_seasons_amount(tv_show)
-    ratings = fetch_ratings(tv_show, seasons_amount)
-    draw_spreadsheet(ratings, seasons_amount)
+    total_seasons = fetch_total_seasons(tv_show)
+    ratings = fetch_ratings(tv_show, total_seasons)
+    draw_spreadsheet(ratings, total_seasons)
