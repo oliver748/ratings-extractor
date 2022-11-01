e2ab43c9c3d6ca78d8e5e9287e8085b58c43eba1
oliver748 <olivereksd@proton.me>
2022-11-01 21:46:07 +0100

    formatting, more readability, variable name changes

--- draw.py
+++ draw.py
@@ -5,45 +5,52 @@ from openpyxl.styles.borders import Border, Side
 from openpyxl import Workbook
 
 FILE_NAME = "episode_ratings.xlsx"
-BG_CLR = "262626" # background color
+BG_HEX = "262626"  # background color
 
 # color for cells and text depending on what episode and season it is
-LOW_RATING_CLR= "B04A4A"  # light red,  clr = color
-MID_RATING_CLR= "F5862B" # orange,  clr = color
-HIGH_RATING_CLR= "1E9A47" # green,  clr = color
-MAX_RATING_CLR = "31869B" # turquoise,  clr = color
+LOW_RATING_HEX = "B04A4A"  # light red
+MID_RATING_HEX = "F5862B"  # orange
+HIGH_RATING_HEX = "1E9A47"  # green
+MAX_RATING_HEX = "31869B"  # turquoise
 
-RATING_CELL_CLR = "262626" # clr = color
-RATING_FONT_CLR = "FFFFFF" # clr = color
-CELL_WIDTH = 5 # width between each cell on the x axis (columns)
+RATING_CELL_HEX = "262626"  # black-ish
+RATING_FONT_HEX = "FFFFFF"  # white
+CELL_WIDTH = 5  # width between each cell on the x axis (columns)
 BORDER = True
-THIN_BORDER = Border(left=Side(style='thin'), 
-                    right=Side(style='thin'), 
-                    top=Side(style='thin'), 
-                    bottom=Side(style='thin'))
+THIN_BORDER = Border(
+    left=Side(style="thin"),
+    right=Side(style="thin"),
+    top=Side(style="thin"),
+    bottom=Side(style="thin"))
+
 
 def fill_cell(cell, value):
-    v = float(value)
-    if v < 7: cell.fill = PatternFill("solid", start_color=LOW_RATING_CLR)
-    elif v < 8: cell.fill = PatternFill("solid", start_color=MID_RATING_CLR)
-    elif v < 10: cell.fill = PatternFill("solid", start_color=HIGH_RATING_CLR)
-    elif v == 10: cell.fill = PatternFill("solid", start_color=MAX_RATING_CLR)
+    value = float(value)
+    if value < 7:
+        cell.fill = PatternFill("solid", start_color=LOW_RATING_HEX)
+    elif value < 8:
+        cell.fill = PatternFill("solid", start_color=MID_RATING_HEX)
+    elif value < 10:
+        cell.fill = PatternFill("solid", start_color=HIGH_RATING_HEX)
+    elif value == 10:
+        cell.fill = PatternFill("solid", start_color=MAX_RATING_HEX)
+
 
 def draw_spreadsheet(ratings, amount):
     wb = Workbook()
-    ws = wb.active 
-    
+    ws = wb.active
+
     # col and row starts at 2 because the first ones are to be left untouched
-    col_count = 2 
+    col_count = 2
     for season in ratings:
-        row_count = 2 
-        cell = ws.cell(row=1, column=season+1, value=season)
-        cell.alignment = Alignment(horizontal='center') # centers cells
-        cell.fill = PatternFill("solid", start_color=RATING_CELL_CLR)
-        cell.font = Font(color=RATING_FONT_CLR)
+        row_count = 2
+        cell = ws.cell(row=1, column=season + 1, value=season)
+        cell.alignment = Alignment(horizontal="center")  # centers cells
+        cell.fill = PatternFill("solid", start_color=RATING_CELL_HEX)
+        cell.font = Font(color=RATING_FONT_HEX)
         for episode in ratings[season]:
             cell = ws.cell(row=row_count, column=col_count, value=episode)
-            cell.alignment = Alignment(horizontal='center')
+            cell.alignment = Alignment(horizontal="center")
             fill_cell(cell, episode)
             row_count += 1
         col_count += 1
@@ -53,12 +60,12 @@ def draw_spreadsheet(ratings, amount):
     max_episodes = max(episodes_list)
 
     # put the correct number of episodes on the columns
-    for i in range(max_episodes+1):
-        cell = ws.cell(row=i+1, column=1, value=i)
-        cell.alignment = Alignment(horizontal='center') # centers rest of cells
-        cell.fill = PatternFill("solid", start_color=RATING_CELL_CLR)
-        cell.font = Font(color=RATING_FONT_CLR)
-    ws.cell(row=1, column=1, value="") # clears first cell (A1)
+    for i in range(max_episodes + 1):
+        cell = ws.cell(row=i + 1, column=1, value=i)
+        cell.alignment = Alignment(horizontal="center")  # centers rest of cells
+        cell.fill = PatternFill("solid", start_color=RATING_CELL_HEX)
+        cell.font = Font(color=RATING_FONT_HEX)
+    ws.cell(row=1, column=1, value="")  # clears first cell (A1)
 
     # sets the correct width for each cell
     dim_holder = DimensionHolder(worksheet=ws)
@@ -68,15 +75,15 @@ def draw_spreadsheet(ratings, amount):
     ws.column_dimensions = dim_holder
 
     # formats the rest: background color and border
-    for col in range(amount+1):
-        for row in range(max_episodes+1):
-            cell = ws.cell(row=row+1, column=col+1)
-            fill = str(cell.fill)[136:144] # extracts bg color from cell
-            if fill == "00000000": # fills bg color if the episode doesnt exist
-                cell.fill = PatternFill("solid", start_color=BG_CLR)
+    for col in range(amount + 1):
+        for row in range(max_episodes + 1):
+            cell = ws.cell(row=row + 1, column=col + 1)
+            fill = str(cell.fill)[136:144]  # extracts bg color from cell
+            if fill == "00000000":  # fills bg color if the episode doesnt exist
+                cell.fill = PatternFill("solid", start_color=BG_HEX)
             if BORDER == True:
                 cell.border = THIN_BORDER
 
-    wb.save(filename = FILE_NAME)
-    print("\nEverything is done", 
+    wb.save(filename=FILE_NAME)
+    print("\nEverything is done",
         f"— you can now close this window and open up '{FILE_NAME}'")
